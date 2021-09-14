f20855843bdc3482ce7c18d18172ce78b394a1a8
Nikita <balykin230195@gmail.com>
2021-09-14 15:52:45 +0300

    release 1

--- script.js
+++ script.js
@@ -1,7 +1,15 @@
-let student = {
-  firstName: 'Petr', 
-  lastName: 'Perviy'
-  }
+let student = {firstName: 'Petr', lastName: 'Perviy'};
+
+let student1 = {firstName: 'Marin', lastName: 'Krakova'};
+  
+let student2 = {firstName: 'Paul', lastName: 'George'};
 student.firstName = 'Petya'
 delete student.lastName
-console.log(student)
+
+let group = []
+if (student.lastName === undefined){
+  student.lastName = ""
+}
+group.push(`${student.firstName +""+ student.lastName}, ${student1.firstName +" "+ student1.lastName}, ${student2.firstName +" "+ student2.lastName}`)
+
+console.log(group)
