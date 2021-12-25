32c1640b431e1716729e7d03ea08e91eb675dc68
Dmitriy Rudakov <cska2004@gmail.com>
2021-12-25 22:55:09 +0300

    Release 1. Создан массив объектов

--- script.js
+++ script.js
@@ -12,3 +12,14 @@ console.log("Step 2.", student);
 delete student.firstName;
 
 console.log("Step 3.", student);
+
+let group = [];
+group.push(student);
+
+console.log("Array group :", group);
+
+let student2 = { firstName: "Ivan", lastName: "Ivanov" };
+group.push(student2);
+group.push({ firstName: "Petr", lastName: "Petrov" });
+
+console.log("Array group modified:", group);
